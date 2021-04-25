3da0b33326c55587316eda34b858754d8c279b78
muzhi <muzhi@gamil.com>
2021-04-25 20:19:51 +0800

    feat: （企业）注册，表单判断完成

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 App({
   globalData: {
-    now: (new Date()).toLocaleString()
+    now: (new Date()).toLocaleString(),
+    
+    companyName: {
+      minLength: 2,
+      maxLength: 64
+    },
+    password: {
+      minLength: 6,
+      maxLength: 20
+    },
+    username: {
+      minLength: 5,
+      maxLength: 18
+    },
+    codeVerify: {
+      length: 6
+    }
+
+  },
+
+  /**
+   * @commet: 检查电话号码
+   * @return: true: 是; false: 不是;
+   */
+  checkPhone: function (phone) {
+    let reg = /^1[3-9][0-9]{9}$/;
+    return reg.test(phone);
   }
+
+
 });
--- pages/index/register.js
+++ pages/index/register.js
@@ -1,37 +1,219 @@
 // pages/index/register.js
+/**
+ * 用来获取全局变量 app
+ */
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    registerDisabled: true,
+    registerDisabled: false,
+
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    companyName: '',
+    phone: '',
+    codeVerify: '',
+
   },
 
-  phoneInput: function (argu) {
-    let phoneInput = argu.detail.value;
-    /**
-     * 判断输入的字符串是否符合号码的格式
-     */
-    let judgePhone = phoneInput.match(/^1[3-9][0-9]{9}$/)
-    if (judgePhone) {
-      this.setData({
-        registerDisabled: false
-      })
-    } else {
-      /**
-       * 不要漏掉一种情况：格式正确后，然后修改
-       */
-      this.setData({
-        registerDisabled: true
+  usernameCheck: function (argu) {
+    let username = argu.detail.value;
+    let usernameLength = username.length;
+    this.setData({
+      username: username
+    })
+    if (usernameLength < app.globalData.username.minLength 
+      || usernameLength > app.globalData.username.maxLength) {
+      wx.showToast({
+        title: '用户名格式不正确',
+        icon: 'fail',
+        duration: 600,
       })
     }
   },
 
-  registerTap: function () {
-    console.log("register tap..");
+  passwordCheck: function (argu) {
+    let password = argu.detail.value;
+    let passwordLength = password.length;
+
+    /**
+     * 写在外面，可以避免来回修改~
+     */
+    this.setData({
+      password: password
+    })
+
+    if (passwordLength < app.globalData.password.minLength 
+      || passwordLength > app.globalData.password.maxLength) {
+      wx.showToast({
+        title: '密码格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+    }
   },
 
+  passwordConfirmCheck: function (argu) {
+    let passwordConfirm = argu.detail.value;
+    this.setData({
+      passwordConfirm: passwordConfirm
+    });
+    if (this.data.password !== this.data.passwordConfirm) {
+      wx.showToast({
+        title: '密码不相同',
+        icon: 'fail',
+        duration: 600,
+      })
+    }
+  },
+
+  companyNameCheck: function (argu) {
+    let companyName = argu.detail.value;
+    this.setData({
+      companyName: companyName
+    })
+
+    if (companyName.length < app.globalData.companyName.minLength 
+      || companyNam.length > app.globalData.companyName.minLength) {
+      wx.showToast({
+        title: '企业名格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+    }
+  },
+
+
+  phoneCheck: function (argu) {
+    let phone  = argu.detail.value;
+    let checkPhone = app.checkPhone(phone);
+
+    this.setData({
+      phone: phone
+    });
+
+    if (!checkPhone) {
+      wx.showToast({
+        title: '手机号不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+    }
+  },
+
+  msgVerify: function (argu) {
+    let codeVerify  = argu.detail.value;
+
+    this.setData({
+      codeVerify: codeVerify
+    });
+
+    if (codeVerify.length !== 6) {
+      wx.showToast({
+        title: '验证码格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+    }
+  },
+
+
+  msgVerify: function (argu) {
+    let codeVerify  = argu.detail.value;
+
+    this.setData({
+      codeVerify: codeVerify
+    });
+
+    if (codeVerify.length !== app.globalData.codeVerify.length) {
+      wx.showToast({
+        title: '验证码格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+    }
+  },
+
+
+
+
+  registerTap: function () {
+
+    let companyNameLength = this.data.companyName.length;
+    let checkPhone = app.checkPhone(this.data.phone);
+    let passwordLength = this.data.password.length;
+    let usernameLength = this.data.username.length;
+
+    if (usernameLength < app.globalData.username.minLength 
+      || usernameLength > app.globalData.username.maxLength) {
+      wx.showToast({
+        title: '用户名格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+      return;
+    }
+
+    if (passwordLength < app.globalData.password.minLength 
+      || passwordLength > app.globalData.password.maxLength) {
+      wx.showToast({
+        title: '密码格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+      return;
+    }
+    if (this.data.password !== this.data.passwordConfirm) {
+      wx.showToast({
+        title: '密码不相同',
+        icon: 'fail',
+        duration: 600,
+      })
+      return;
+    }
+
+    if ( companyNameLength < app.globalData.companyName.minLength 
+      || companyNameLength > app.globalData.companyName.maxLength) {
+      wx.showToast({
+        title: '企业名格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+      return;
+    }
+
+    if (!checkPhone) {
+      wx.showToast({
+        title: '手机号不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+      return;
+    };
+
+    if (this.data.codeVerify.length !== app.globalData.codeVerify.length) {
+      wx.showToast({
+        title: '验证码格式不正确',
+        icon: 'fail',
+        duration: 600,
+      })
+      return;
+    };
+
+    wx.showToast({
+      title: '注册成功',
+      icon: 'success',
+      duration: 600,
+    })
+    return;
+  },
+
+  
 
   /**
    * 生命周期函数--监听页面加载

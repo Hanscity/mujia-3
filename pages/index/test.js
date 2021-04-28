4343a28574b98d32f399e88aa1c43e952d4b139f
muzhi <muzhi@gamil.com>
2021-04-28 19:07:12 +0800

    feat: 下拉框，数据对象更新为普通对象，完成

--- pages/index/test.js
+++ pages/index/test.js
@@ -4,10 +4,22 @@ Page({
    */
   data: {
     shows: false, //控制下拉列表的显示隐藏，false隐藏、true显示
-    selectDatas: ['稻谷', '大米', '稻米副产品',  '小麦', '小麦副产品', 
-                  '玉米', '玉米副产品', '大豆', '大豆副产品', 
-                  '杂粮', '杂粮副产品'], //下拉列表的数据
-    indexs: 0, //选择的下拉列 表下标,
+    selectDatas: {
+      1: '稻谷',
+      2: '大米',
+      3: '稻米副产品',
+      101: '小麦',
+      102: '小麦副产品',
+      201: '玉米',
+      202: '玉米副产品',
+      301: '大豆',
+      302: '大豆副产品',
+      401: '杂粮',
+      402: '杂粮副产品'
+  }, //下拉列表的数据
+    indexs: 1, //选择的下拉列 表下标,
+    selectDatasLength: 0,
+
   },
 
   // 点击下拉显示框
@@ -26,5 +38,16 @@ Page({
     });
   },
 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
+    let selectDatasLength = Object.getOwnPropertyNames(this.data.selectDatas).length;
+
+    this.setData({
+      selectDatasLength: selectDatasLength
+    })
+  },
+  
 })

45b57153d49bdcd7361b4f5fc3849a829d8c4173
muzhi <muzhi@gamil.com>
2021-05-01 18:01:34 +0800

    feat: 当前的 test 页面时微信授权登录功能

--- pages/index/test.js
+++ pages/index/test.js
@@ -1,53 +1,47 @@
+//index.js
 Page({
-  /**
-   * 页面的初始数据
-   */
+  // 页面的初始数据
   data: {
-    shows: false, //控制下拉列表的显示隐藏，false隐藏、true显示
-    selectDatas: {
-      1: '稻谷',
-      2: '大米',
-      3: '稻米副产品',
-      101: '小麦',
-      102: '小麦副产品',
-      201: '玉米',
-      202: '玉米副产品',
-      301: '大豆',
-      302: '大豆副产品',
-      401: '杂粮',
-      402: '杂粮副产品'
-  }, //下拉列表的数据
-    indexs: 1, //选择的下拉列 表下标,
-    selectDatasLength: 0,
-
+    isShowUserName: false,
+    userInfo: null,
   },
 
-  // 点击下拉显示框
-  selectTaps() {
-    this.setData({
-      shows: !this.data.shows,
-    });
-  },
-  // 点击下拉列表
-  optionTaps(e) {
-    let Indexs = e.currentTarget.dataset.index; //获取点击的下拉列表的下标
-    console.log(Indexs)
-    this.setData({
-      indexs: Indexs,
-      shows: !this.data.shows
-    });
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-    let selectDatasLength = Object.getOwnPropertyNames(this.data.selectDatas).length;
-
-    this.setData({
-      selectDatasLength: selectDatasLength
+  
+  getUserProfile: function(e) {
+    wx.getUserProfile({
+      /**
+       * parameter error: parameter.desc should be String instead of Undefined;
+       * 简而言之，就是 desc 属性必不可少
+       */
+      desc: '用于完善会员资料',
+      success: (res) => {
+        console.log("获取用户信息成功", res);
+        let user = res.userInfo;
+        // wx.setStorageSync('user', user);
+        this.setData({
+          isShowUserName: true,
+          userInfo: user,
+        })
+      },
+      fail: res => {
+        console.log("获取用户信息失败", res);
+      }
     })
   },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  // onShow(options) {
+  //   this.getUserProfile();
+  //   let user = wx.getStorageSync('user');
+  //   if (user && user.nickname) {
+  //     this.setData({
+  //       isShowUserName: true,
+  //       userInfo: user,
+  //     })
+  //   }
+  // },
+
   
 })
